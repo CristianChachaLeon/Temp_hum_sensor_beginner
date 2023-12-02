299bd1708926c98b3ec7b174a533b42a28c2c6bb
Cristian Chacha <cristianchacha@gmail.com>
2023-12-02 16:50:29 +0100

    Deleted macro Sensor and added debug option

--- temp_hum_sensor/temp_hum_sensor.ino
+++ temp_hum_sensor/temp_hum_sensor.ino
@@ -3,7 +3,7 @@
 
 #define DHT_DEVICE 2
 #define DHT_TYPE DHT22
-
+//#define DEBUG
 DHT dht22(DHT_DEVICE, DHT_TYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2); //
 
@@ -14,17 +14,19 @@ void show_temp_and_hum_in_lcd(LiquidCrystal_I2C *lcd_obj, float temp, float hum)
 {
   lcd_obj->setCursor(0, 0);
   lcd_obj->print("Temp ");
-  lcd_obj->print(hum);
+  lcd_obj->print(temp);
   lcd_obj->print("C");
   lcd_obj->setCursor(0, 1);
   lcd_obj->print("Humidity ");
-  lcd_obj->print(temp);
+  lcd_obj->print(hum);
   lcd_obj->print("%");
 }
 void setup()
-{
+{ 
+  #ifdef DEBUG
   Serial.begin(9600);
   Serial.println("Init...");
+  #endif
   dht22.begin();
   lcd.init();
   lcd.backlight();
@@ -32,16 +34,14 @@ void setup()
 
 void loop()
 {
-#ifdef SENSOR
   humidity = dht22.readHumidity();
   temperature = dht22.readTemperature();
-#else
-  humidity = 10+ cnt;
-  temperature = 50 + cnt;
-  cnt++;
-#endif
-  Serial.println(humidity);
+  #ifdef DEBUG
+  Serial.print("Temp: ");
   Serial.println(temperature);
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  #endif
   show_temp_and_hum_in_lcd(&lcd, temperature, humidity);
   delay(1000);
 }
